7ceee375723fb41c04af640ec1bff6d5c01c867b
jaemin <jjmmll0727@gmail.com>
2021-05-12 21:33:54 +0900

    90% success / todo: index out of range error & more train for eye image

--- research/Xception-eye-blink/src/check.py
+++ research/Xception-eye-blink/src/check.py
@@ -21,6 +21,8 @@ classes = [
 ]
 
 def preprocess(image_path):
+    
+    eye_list = []
     transform_test = transforms.Compose([
         transforms.ToTensor()
     ])
@@ -32,23 +34,34 @@ def preprocess(image_path):
         minSize=(1, 1),
         flags=cv2.CASCADE_SCALE_IMAGE
     )
+    ## 이부분 수정해야해 --> 얼굴이 탐지가 되지 않더라도 프로그램이 계속 돌아가도록...
     if len(faces) == 0:
         print('no face found')
-        face = cv2.resize(image, shape)
-        return None, face
+        main()
+        
+        # face = cv2.resize(image, shape)
+        # return None, face
     else: # img에서 발견된 얼굴이 한개라도 있다면.
         (x, y, w, h) = faces[0] # img에서 첫번째로 발견된 얼굴의 위치 좌표
-        
         face = image[y:y + h, x:x + w]
-        (x, y, w, h) = image_path['left']
+        eyes = eye_cascade.detectMultiScale(face)
+        for(ex, ey, ew, eh) in eyes:
+            eye_list.append((ex, ey, ew, eh))
+            print(eye_list)
+
+
+        #(x, y, w, h) = image_path['left'] eye_list[0]
+        (x, y, w, h) = eye_list[0] 
+        #### 이 부분에서 만약에 list index out of range --> default로 0혹은 근사값으로 대체해주는 작업 필요
         left_eye = face[y:y + h, x:x + w]
         left_eye = cv2.resize(left_eye, shape)
-        (x, y, w, h) = image_path['right']
+        #(x, y, w, h) = image_path['right'] eye_list[1]
+        (x, y, w, h) = eye_list[1]
         right_eye = face[y:y + h, x:x + w]
         right_eye = cv2.resize(right_eye, shape)
         return transform_test(Image.fromarray(left_eye).convert('L')), \
-               transform_test(Image.fromarray(right_eye).convert('L')), \
-               left_eye, right_eye, cv2.resize(face, (48,48))
+            transform_test(Image.fromarray(right_eye).convert('L')), \
+            left_eye, right_eye, cv2.resize(face, (48,48))
 
 
 def get_gradient_image(gradient):
@@ -103,7 +116,8 @@ def guided_backprop_eye(image, name, net):
     grad_cam_image = get_gradcam_image(gcam=regions[0, 0], raw_image=image[name + '_raw'])
     guided_gradcam_image = get_gradient_image(torch.mul(regions, gradients)[0])
     guided_gradcam_image = cv2.merge((guided_gradcam_image, guided_gradcam_image, guided_gradcam_image))
-    print(image['path'],classes[actual_status.data], probs.data[:,0] * 100)
+    #print(image['path'],classes[actual_status.data], probs.data[:,0] * 100)
+    print(classes[actual_status.data], probs.data[:,0] * 100)
 
     return cv2.hconcat(
         [image[name + '_raw'], prob_image, guided_bpg_image, grad_cam_image, guided_gradcam_image])
@@ -123,7 +137,7 @@ def guided_backprop(images, model_name):
     checkpoint = torch.load(os.path.join('../trained', model_name), map_location=torch.device('cpu'))
     net.load_state_dict(checkpoint['net'])
     net.eval()
-    summary(net, (1, shape[0], shape[1]))
+    #summary(net, (1, shape[0], shape[1])) 이 부분이 모델을 출력주는 torchsummary의 내장함수
 
     result_images = []
     for index, image in enumerate(images):
@@ -136,15 +150,29 @@ def guided_backprop(images, model_name):
     cv2.imwrite('../test/guided_gradcam.jpg',cv2.resize(cv2.vconcat(result_images), None, fx=2,fy=2))
 
 def main():
-    guided_backprop(
-        images=[
-            {'path': '../test/open_man.jpg', 'right': (17,26,21,21), 'left': (47,24,22,22)},
-            {'path': '../test/closed_man.jpg', 'right': (27,43,32,32), 'left': (92,38,43,43)},
-            {'path': '../test/open_woman.jpg', 'right': (50,82,77,77), 'left': (184,65,78,78)},
-            {'path': '../test/closed_woman.jpg','right': (28,30,46,46), 'left': (83,34,42,42)},
-        ],
-        model_name='model_11_96_0.1256.t7'
-    )
+    cap = cv2.VideoCapture(0)
+    while cap.isOpened():
+        ret, img_ori = cap.read()
+
+        if not ret:
+            break
+
+        img_ori = cv2.resize(img_ori, dsize=(0, 0), fx=0.5, fy=0.5)
+
+        img = img_ori.copy()
+        faces = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        cv2.imwrite('../test/sample.jpg', faces)
+
+        guided_backprop(
+            images=[
+                # {'path': '../test/open_man.jpg', 'right': (17,26,21,21), 'left': (47,24,22,22)},
+                # {'path': '../test/closed_man.jpg', 'right': (27,43,32,32), 'left': (92,38,43,43)},
+                # {'path': '../test/open_woman.jpg', 'right': (50,82,77,77), 'left': (184,65,78,78)},
+                # {'path': '../test/closed_woman.jpg','right': (28,30,46,46), 'left': (83,34,42,42)},
+                {'path': '../test/sample.jpg'},
+            ],
+            model_name='model_11_96_0.1256.t7'
+        )
 
 
 if __name__ == "__main__":
