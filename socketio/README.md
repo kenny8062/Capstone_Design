30485ba12c3300e2f44a1881461657d2c5612c18
jaemin <jjmmll0727@gmail.com>
2021-06-07 16:07:05 +0900

    socket io setted2

---
+++ socketio/README.md
@@ -0,0 +1,7 @@
+<div style = "font-size: 18px; line-height: 25px; text-align: left">
+socketio protocol 
+</div>
+<ul>
+<li>for bi-directional protocol</li>
+<li>response 없이도 반복적으로 request를 보내기 위해 양방향 프로토콜을 사용한다.</li>
+</ul>
---
+++ socketio/socketserver.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var socket = require('socket.io');
+
+// App Setup
+var app = express();
+// Create server and listen to specific port number
+var server = app.listen(4000, function(){
+	console.log('Listening to requests on port 4000');
+});
+
+// Static files or middle-ware to serve public folder to client
+app.use(express.static('public'));
+
+// Socket setup; provide server reference to work with
+var io = socket(server);
+
+// Listening to an connection event; socket parameter refers to created/particular socket
+io.on('connection', function(socket){
+	// Print socket id on new connection
+	console.log(`made socket connection, ${socket.id}`);
+
+	// Listen for chat message being sent from client
+	socket.on('chat1', function(data){
+		console.log(data)
+		// Send received chat message to all connected clients
+		io.sockets.emit('chat2',data);
+	});
+
+    socket.on('disconnect', () => {
+        console.log(`Socket disconnected : ${socket.id}`)
+    })
+});
